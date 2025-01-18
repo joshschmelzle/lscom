824d3ae52987e47baeb644b5fe32bdb83d1879e8
Josh Schmelzle <josh@joshschmelzle.com>
2025-01-18 14:51:48 -0500

    linux serial port permissions checking

--- lscom/app.py
+++ lscom/app.py
@@ -11,6 +11,8 @@ main app code
 """
 
 import glob
+import grp
+import os
 import sys
 
 try:
@@ -23,6 +25,42 @@ except ModuleNotFoundError:
 class lscom:
     """Main application class."""
 
+    def check_serial_permissions(self):
+        """
+        Check if current user has permissions for serial port access on Linux.
+
+        Add to dialout:
+            sudo usermod -a -G dialout $USER
+
+        Remove from dialout:
+            sudo gpasswd -d $USER dialout
+
+        :returns:
+            Tuple: (bool, message)
+        """
+        if not sys.platform.startswith("linux"):
+            return True, "Permission check required"
+
+        try:
+            dialout = grp.getgrnam("dialout")
+            groups = os.getgroups()
+            user = os.getlogin()
+            if dialout.gr_gid in groups:
+                return True, f"{user} has dialout group access"
+            else:
+                return (
+                    False,
+                    f"""
+    {user} is not in the dialout group. To fix:
+    1. Run: sudo usermod -a -G dialout {user}
+    2. Log out and back in for the changes to take effect
+    """,
+                )
+        except KeyError:
+            return False, "dialout group not found"
+        except Exception as error:
+            return False, f"Error checking permissions: {str(error)}"
+
     def get_active_serial_port_names(self):
         """Lists serial port names
 
@@ -31,6 +69,9 @@ class lscom:
         :returns:
             A list of the serial ports available on the system
         """
+        has_permissions, message = self.check_serial_permissions()
+        if not has_permissions:
+            print(message)
         if sys.platform.startswith("win"):
             ports = ["COM%s" % (i + 1) for i in range(256)]
         elif sys.platform.startswith("linux") or sys.platform.startswith("cygwin"):
