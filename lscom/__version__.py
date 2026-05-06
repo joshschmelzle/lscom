ca18f672c94445f40918245287f5c32f1de88d7d
Josh Schmelzle <schmelzle@hpe.com>
2026-05-06 12:15:01 -0400

    fix: list real macOS USB serial ports and exclude ports already in use

--- lscom/__version__.py
+++ lscom/__version__.py
@@ -8,7 +8,7 @@
 __title__ = "lscom"
 __description__ = "list available active COM ports"
 __url__ = "https://github.com/joshschmelzle/lscom"
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 __author__ = "Josh Schmelzle"
 __author_email__ = "josh@joshschmelzle.com"
 __license__ = "MIT"
